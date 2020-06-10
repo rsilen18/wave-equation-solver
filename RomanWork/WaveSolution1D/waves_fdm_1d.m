2f09557711bde4a44e5856b125d0d72a7b6d1c5c
Roman Silen <romansilen@gmail.com>
2020-06-10 15:46:27 -0400

    refactored code

--- RomanWork/WaveSolution1D/waves_fdm_1d.m
+++ RomanWork/WaveSolution1D/waves_fdm_1d.m
@@ -2,60 +2,96 @@
 % BCs: u(a,t) = l(t), u_x(b,t) = r(t)
 % ICs: u(x,0) = u_0(x), u_t(x,0) = v_0(x)
 
-function [u,e] = waves_fdm_1d(N,sigma,c,t_f,icase,plot_flag,order)
-    [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase);
-    dx = (b-a)/N;
-    nt = t_f/(sigma*dx/c);
+function [u,e] = waves_fdm_1d(def,sigma,plot_flag,order)
+    dx = (def.b-def.a)/def.N;
+    nt = def.t_f/(sigma*dx/def.c);
     nt = ceil(nt);
-    dt = t_f/nt;
-    sigma = c*dt/dx;
+    dt = def.t_f/nt;
+    sigma = def.c*dt/dx;
+    
+    % to account for index offset in 4th order
+    function index = j_(i)
+        index = i+3;
+    end
     
     if (order == 2)
-        x = linspace(a,b,N+1);
+        x = linspace(def.a,def.b,def.N+1);
         % initial conditions
-        unm1 = zeros(1,N+1);
-        for i = 1:N+1
-            unm1(i) = u_0(x(i));
+        unm1 = zeros(1,def.N+1);
+        for j = 1:def.N+1
+            unm1(j) = def.f(x(j));
         end
-        unm1(N+1) = (4*unm1(N)-unm1(N-1)-2*right(0)*dx)/3;
-        un = zeros(1,N+1);
-        un(1) = left(dt);
-        for i = 2:N
-           un(i) =  (1-sigma^2)*unm1(i)+dt*v_0(x(i))+sigma^2/2*(unm1(i-1)+unm1(i+1));
+        unm1(def.N+1) = (4*unm1(def.N)-unm1(def.N-1)-2*def.right(0)*dx)/3;
+        un = zeros(1,def.N+1);
+        un(1) = def.left(dt);
+        for j = 2:def.N
+           un(j) =  (1-sigma^2)*unm1(j)+dt*def.g(x(j))+sigma^2/2*(unm1(j-1)+unm1(j+1));
         end
-        un(N+1) = (4*un(N)-un(N-1)-2*right(dt)*dx)/3;
+        un(def.N+1) = (4*un(def.N)-un(def.N-1)-2*def.right(dt)*dx)/3;
         % time loop
-        j = 2;
-        while j*dt <= t_f
-            unp1 = zeros(1,N+1);
-            unp1(1) = left(j*dt);
-            for i = 2:N
-                unp1(i) = 2*un(i)-unm1(i)+sigma^2*(un(i+1)-2*un(i)+un(i-1));
+        n = 2;
+        while n*dt <= def.t_f
+            unp1 = zeros(1,def.N+1);
+            unp1(1) = def.left(n*dt);
+            for j = 2:def.N
+                unp1(j) = 2*un(j)-unm1(j)+sigma^2*(un(j+1)-2*un(j)+un(j-1));
             end
-            unp1(N+1) = (4*unp1(N)-unp1(N-1)-2*right(j*dt)*dx)/3;
+            unp1(def.N+1) = (4*unp1(def.N)-unp1(def.N-1)-2*def.right(n*dt)*dx)/3;
             % optionally plot
             if (plot_flag)
                plot(x,unp1);
-               xlim([a b]);
+               xlim([def.a def.b]);
                ylim([-2 2]);
                xlabel("x step");
                ylabel("displacement: u(x,t)");
-               str = sprintf("1-D Wave at t=%.2f",j*dt);
+               str = sprintf("1-D Wave at t=%.2f",n*dt);
                title(str);
                pause(.0001);
             end
             unm1 = un;
             un = unp1;
-            j = j+1;
+            n = n+1;
         end
         % assignment of u at final time step
         u = unp1;
-        e = abs(u-waves_analytic_1d(u_0,v_0,c,x,t_f));
+        e = abs(u-waves_analytic_1d(def.f,def.g,def.c,x,def.t_f));
         
     elseif (order == 4)
-        x = linspace(a-2*dx,b+2*dx,N+4);
-        j = 1;
+        x = linspace(def.a-2*dx,def.b+2*dx,def.N+5);
+        
         % first time step
-        u1 = 2;
+        unm1 = waves_analytic_1d(def.f,def.g,def.c,x,0);
+        un = waves_analytic_1d(def.f,def.g,def.c,x,dt);
+        % remaining steps
+        n = 2;
+        while n*dt <= def.t_f
+            unp1 = zeros(1,def.N+5);
+            unp1(j_(-2)) = waves_analytic_1d(def.f,def.g,def.c,x(j_(-2)),n*dt);
+            unp1(j_(-1)) = waves_analytic_1d(def.f,def.g,def.c,x(j_(-1)),n*dt);
+            unp1(j_(def.N+1)) = waves_analytic_1d(def.f,def.g,def.c,x(j_(def.N+1)),n*dt);
+            unp1(j_(def.N+2)) = waves_analytic_1d(def.f,def.g,def.c,x(j_(def.N+2)),n*dt);
+            for j = 0:def.N
+                order2 = un(j_(j+1))-2*un(j_(j))+un(j_(j-1));
+                order4 = un(j_(j+2))-4*un(j_(j+1))+6*un(j_(j))-4*un(j_(j-1))+un(j_(j-2));
+                unp1(j_(j)) = 2*un(j_(j))-unm1(j_(j))+sigma^2*(order2-1/12*order4)+1/12*sigma^4*order4;
+            end
+            % optionally plot
+            if (plot_flag)
+               plot(x,unp1);
+               xlim([def.a def.b]);
+               ylim([-2 2]);
+               xlabel("x step");
+               ylabel("displacement: u(x,t)");
+               str = sprintf("1-D Wave at t=%.2f",n*dt);
+               title(str);
+               pause(.0001);
+            end
+            unm1 = un;
+            un = unp1;
+            n = n+1;
+        end
+        % assignment of u at final time step
+        u = unp1(j_(0):j_(def.N));
+        e = abs(u-waves_analytic_1d(def.f,def.g,def.c,x(j_(0):j_(def.N)),def.t_f));
     end
 end
--- RomanWork/WaveSolution1D/waves_fdm_1d_defs.m
+++ RomanWork/WaveSolution1D/waves_fdm_1d_defs.m
@@ -2,15 +2,21 @@
 
 % a = left bound on x
 % b = right bound on x
+% c = wave number
+% N = resolution in x-dimension
+% t_f = final time
 % u_0 = initial displacement
 % v_0 = initial velocity
 % left = left boundary condition: u(0,t)
 % right = right boundary condition: u_x(1,t)
-function [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
+function [a,b,c,N,t_f,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
     switch icase
         case 1
             a = 0;
             b = 3*pi/2;
+            c = 1;
+            N = 100;
+            t_f = 10;
             u_0 = @(x) sin(x);
             v_0 = @(x) -sin(x);
             left = @(t) 0;
@@ -18,6 +24,9 @@ function [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
         case 2
             a = 0;
             b = 3*pi/2;
+            c = 1;
+            N = 100;
+            t_f = 10;
             u_0 = @(x) sin(x);
             v_0 = @(x) 0;
             left = @(t) 0;
@@ -25,6 +34,9 @@ function [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
         case 3
             a = 0;
             b = 1;
+            c = 1;
+            N = 100;
+            t_f = 10;
             u_0 = @(x) 0;
             v_0 = @(x) x;
             left = @(t) 0;
@@ -32,6 +44,9 @@ function [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
         case 4
             a = 0;
             b = 1;
+            c = 1;
+            N = 100;
+            t_f = 10;
             u_0 = @(x) sin(x);
             v_0 = @(x) -sin(x);
             left = @(t) t;
@@ -39,6 +54,9 @@ function [a,b,u_0,v_0,left,right] = waves_fdm_1d_defs(icase)
         case 5
             a = 0;
             b = 1;
+            c = 1;
+            N = 100;
+            t_f = 10;
             u_0 = @(x) sin(x);
             v_0 = @(x) -sin(x);
             left = @(t) 0;
