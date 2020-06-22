88ec2b9d78de47fe9d1acdbb0d979d7954cd8bb7
Roman Silen <romansilen@gmail.com>
2020-06-22 18:09:13 -0400

    Added 4th order BCs code

--- RomanWork/WaveSolution1D/waves_fdm_1d.m
+++ RomanWork/WaveSolution1D/waves_fdm_1d.m
@@ -9,105 +9,109 @@ function [u,e] = waves_fdm_1d(def,sigma,plot_flag,order)
     dt = def.t_f/nt;
     sigma = def.c*dt/dx;
     
-    % to account for index offset in 4th order
-    function index = x_(i)
-        index = i+3;
+
+    x = linspace(def.a-dx*order/2, def.b+dx*order/2, def.N+order+1);
+    unm1 = ICs(def,x);
+    un = first_time_step(def,sigma,x,order,dt,unm1);
+    un = BCs(def,sigma,x,order,dx,dt,un,1);
+    n = 2;
+    while n*dt <= def.t_f
+        unp1 = main_time_step(sigma,x,order,unm1,un);
+        unp1 = BCs(def,sigma,x,order,dx,dt,unp1,n);
+        % optionally plot
+        if (plot_flag)
+            plot(x,unp1);
+            xlim([def.a def.b]);
+            ylim([-2 2]);
+            xlabel("x step");
+            ylabel("displacement: u(x,t)");
+            str = sprintf("1-D Wave at t=%.2f",n*dt);
+            title(str);
+            pause(.0001);
+        end
+        unm1 = un;
+        un = unp1;
+        n = n+1;
     end
+    % assignment of u at final time step
+    u = unp1(1+order/2:size(x,2)-order/2);
+    e = abs(u-waves_analytic_1d(def.f,def.g,def.c,x(1+order/2:size(x,2)-order/2),def.t_f));
     
-    if (order == 2)
-        x = linspace(def.a,def.b,def.N+1);
-        % initial conditions
-        unm1 = zeros(1,def.N+1);
-        for j = 1:def.N+1
+end
+
+%%% set up functions below
+
+% initial conditions
+    function unm1 = ICs(def,x)
+        n = size(x,2);
+        unm1 = zeros(1,n);
+        for j = 1:n
             unm1(j) = def.f(x(j));
         end
-        unm1(def.N+1) = (4*unm1(def.N)-unm1(def.N-1)-2*def.right(0)*dx)/3;
-        un = zeros(1,def.N+1);
-        un(1) = def.left(dt);
-        for j = 2:def.N
-           un(j) =  (1-sigma^2)*unm1(j)+dt*def.g(x(j))+sigma^2/2*(unm1(j-1)+unm1(j+1));
-        end
-        un(def.N+1) = (4*un(def.N)-un(def.N-1)-2*def.right(dt)*dx)/3;
-        % time loop
-        n = 2;
-        while n*dt <= def.t_f
-            unp1 = zeros(1,def.N+1);
-            unp1(1) = def.left(n*dt);
-            for j = 2:def.N
-                unp1(j) = 2*un(j)-unm1(j)+sigma^2*(un(j+1)-2*un(j)+un(j-1));
-            end
-            unp1(def.N+1) = (4*unp1(def.N)-unp1(def.N-1)-2*def.right(n*dt)*dx)/3;
-            % optionally plot
-            if (plot_flag)
-               plot(x,unp1);
-               xlim([def.a def.b]);
-               ylim([-2 2]);
-               xlabel("x step");
-               ylabel("displacement: u(x,t)");
-               str = sprintf("1-D Wave at t=%.2f",n*dt);
-               title(str);
-               pause(.0001);
-            end
-            unm1 = un;
-            un = unp1;
-            n = n+1;
-        end
-        % assignment of u at final time step
-        u = unp1;
-        e = abs(u-waves_analytic_1d(def.f,def.g,def.c,x,def.t_f));
-        
-    elseif (order == 4)
-        x = linspace(def.a-2*dx,def.b+2*dx,def.N+5);
-        
-        % first time step
-        unm1 = zeros(def.N+5);
-        v_0 = zeros(def.N+5);
-        for j = 1:def.N+5
-            unm1(j) = waves_analytic_1d(def.f,def.g,def.c,x(j),0);
-            v_0(j) = def.g(x(j));
-        end
-        un = zeros(def.N+5);
-        un(x_(-2)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(-2)),dt);
-        un(x_(-1)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(-1)),dt);
-        un(x_(def.N+1)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(def.N+1)),dt);
-        un(x_(def.N+2)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(def.N+2)),dt);
-        for j = 0:def.N
-            order2 = unm1(x_(j+1))-2*unm1(x_(j))+unm1(x_(j-1));
-            order4 = unm1(x_(j+2))-4*unm1(x_(j+1))+6*unm1(x_(j))-4*unm1(x_(j-1))+unm1(x_(j-2));
-            order2v = v_0(x_(j+1))-2*v_0(x_(j))+v_0(x_(j-1));
-            order4v = v_0(x_(j+2))-4*v_0(x_(j+1))+6*v_0(x_(j))-4*v_0(x_(j-1))+v_0(x_(j-2));
-            un(x_(j)) = unm1(x_(j))+dt*v_0(x_(j))+sigma^2/2*(order2-1/12*order4)+sigma^2*dt/6*(order2v-1/12*order4v);
-        end
-        % remaining steps
-        n = 2;
-        while n*dt <= def.t_f
-            unp1 = zeros(1,def.N+5);
-            unp1(x_(-2)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(-2)),n*dt);
-            unp1(x_(-1)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(-1)),n*dt);
-            unp1(x_(def.N+1)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(def.N+1)),n*dt);
-            unp1(x_(def.N+2)) = waves_analytic_1d(def.f,def.g,def.c,x(x_(def.N+2)),n*dt);
-            for j = 0:def.N
-                order2 = un(x_(j+1))-2*un(x_(j))+un(x_(j-1));
-                order4 = un(x_(j+2))-4*un(x_(j+1))+6*un(x_(j))-4*un(x_(j-1))+un(x_(j-2));
-                unp1(x_(j)) = 2*un(x_(j))-unm1(x_(j))+sigma^2*(order2-1/12*order4)+1/12*sigma^4*order4;
-            end
-            % optionally plot
-            if (plot_flag)
-               plot(x,unp1);
-               xlim([def.a def.b]);
-               ylim([-2 2]);
-               xlabel("x step");
-               ylabel("displacement: u(x,t)");
-               str = sprintf("1-D Wave at t=%.2f",n*dt);
-               title(str);
-               pause(.0001);
-            end
-            unm1 = un;
-            un = unp1;
-            n = n+1;
-        end
-        % assignment of u at final time step
-        u = unp1(x_(0):x_(def.N));
-        e = abs(u-waves_analytic_1d(def.f,def.g,def.c,x(x_(0):x_(def.N)),def.t_f));
     end
-end
+
+    % fill in boundary conditions
+    % code for Dirichlet left BC, Neumann right BC
+    % n = timestep
+    function un = BCs(def,sigma,x,order,dx,dt,un,n)
+        m = size(x,2);
+        ja = 1+order/2;
+        jb = m-order/2;
+        if (order == 2)
+            un(ja-1) = 2*un(ja)-un(ja+1);
+            un(jb+1) = un(jb-1);
+        elseif (order == 4)
+            % Dirichlet left BC - discrete delta fcn 
+            % u(1) = un(ja-1)
+            % u(2) = un(ja-2)
+            f = @(u) [def.c^2/dx^2*(un(ja+1)-2*un(ja)+u(1) - ...
+                1/12*(un(ja+2)-4*un(ja+1)+6*un(ja)-4*u(1)+u(2)));
+                def.c^4/dx^4*(un(ja+2)-4*un(ja+1)+6*un(ja)-4*u(1)+u(2))];
+            f0 = f([0;0]);
+            f1 = f([1;0]);
+            f2 = f([0;1]);
+            A = [f1-f0, f2-f0];
+            b = -1*f0;
+            u = A\b;
+            un(ja-1) = u(1);
+            un(ja-2) = u(2);
+            
+            % Neumann right BC - "way 1"
+            A = [2/3 -1/12; -2 1];
+            b = [2/3*un(jb-1)-1/12*un(jb-2)+dx*def.right(n*dt);
+                -2*un(jb-1)+un(jb-2)+2*dx/sigma^2*(def.right((n+1)*dt)-...
+                2*def.right(n*dt)+def.right((n-1)*dt))];
+            u = A\b;
+            un(jb+1) = u(1);
+            un(jb+2) = u(2);
+        end
+    end
+
+    % first time step
+    function un = first_time_step(def,sigma,x,order,dt,unm1)
+        n = size(x,2);
+        un = zeros(1,n);
+        for j = 1+order/2:n-order/2
+            if (order == 2)
+                un(j) =  (1-sigma^2)*unm1(j)+dt*def.g(x(j))+sigma^2/2*(unm1(j-1)+unm1(j+1));
+            elseif (order == 4)
+                un(j) = unm1(j) + ...
+                        dt*def.g(x(j)) + ...
+                        sigma^2/2*(unm1(j+1)-2*unm1(j)+unm1(j-1)-1/12*(unm1(j+2)-4*unm1(j+1)+6*unm1(j)-4*unm1(j-1)+unm1(j-2))) + ...
+                        dt*sigma^2/6*(def.g(x(j+1))-2*def.g(x(j))+def.g(x(j-1))-1/12*(def.g(x(j+2))-4*def.g(x(j+1))+6*def.g(x(j))-4*def.g(x(j-1))+def.g(x(j-2)))) + ...
+                        sigma^4/24*(unm1(j+2)-4*unm1(j+1)+6*unm1(j)-4*unm1(j-1)+unm1(j-2));
+            end
+        end
+    end
+
+    % time steps for n >= 2
+    function unp1 = main_time_step(sigma,x,order,unm1,un)
+        n = size(x,2);
+        unp1 = zeros(1,n);
+        for j = 1+order/2:n-order/2
+            unp1(j) = 2*un(j)-unm1(j)+sigma^2*(un(j+1)-2*un(j)+un(j-1));
+            if (order == 4)
+                unp1(j) = unp1(j) + (sigma^4-sigma^2)/12*(un(j+2)-4*un(j+1)+6*un(j)-4*un(j-1)+un(j-2));
+            end
+        end
+    end
--- RomanWork/WaveSolution1D/waves_fdm_1d_driver.m
+++ RomanWork/WaveSolution1D/waves_fdm_1d_driver.m
@@ -10,10 +10,11 @@ plot_flag = true;    % flag for turning on plot animation
 % Original problem
 [u,e] = waves_fdm_1d(def,sigma,plot_flag,order);
 
-hold on
-x = linspace(0,3*pi/2,def.N+1);
-plot(x,waves_analytic_1d(@(x) sin(x),@(x) -sin(x),c,x,def.t_f));
-hold off
+% hold on
+% x = linspace(0,3*pi/2,def.N+1);
+% plot(x,waves_analytic_1d(@(x) sin(x),@(x) -sin(x),c,x,def.t_f));
+% hold off
+% plot(x,waves_tz_1d(def,sigma,true));
 
 % Convergence study
 icase = 1;
@@ -28,10 +29,11 @@ for i = 1:3
     errors(i) = max(e);
 end
 h2 = h.^2;
+h3 = h.^3;
 h4 = h.^4;
 figure(2)
-loglog(h,errors,'o',h,h2,h,h4);
+loglog(h,errors,'o',h,h2,h,h3,h,h4);
 xlabel("h");
 ylabel("|e|_{\infty}");
 title("Truncation Error");
-legend("error","h^2","h^4");
+legend("error","h^2","h^3","h^4");
